8b2debab278dfa475c81803a57ae99d2795d0026
martinakos <martin.tosas@gmail.com>
2017-11-21 18:49:47 +0000

    Testing convolutional autoencoders

---
+++ convolucional_autoencoder.py
@@ -0,0 +1,80 @@
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
+from keras.models import Model
+from keras import backend as K
+
+from keras.datasets import mnist
+
+import numpy as np
+np.set_printoptions(linewidth=10000, precision = 3, edgeitems= 100, suppress=True)
+import matplotlib.pyplot as plt
+plt.ion()
+
+
+input_img = Input(shape=(28, 28, 1))  # adapt this if using `channels_first` image data format
+#Conv2D first argument is the number output of filters in the convolution
+x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
+x = MaxPooling2D((2, 2), padding='same')(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = MaxPooling2D((2, 2), padding='same')(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+encoded = MaxPooling2D((2, 2), padding='same')(x)
+
+# at this point the representation is (4, 4, 8) i.e. 128-dimensional
+
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
+x = UpSampling2D((2, 2))(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = UpSampling2D((2, 2))(x)
+x = Conv2D(16, (3, 3), activation='relu')(x)
+x = UpSampling2D((2, 2))(x)
+decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+
+conv_autoencoder = Model(input_img, decoded)
+conv_autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+
+
+
+if K.image_data_format() == 'channels_last':
+    shape_ord = (28, 28, 1)
+else:
+    shape_ord = (1, 28, 28)
+
+(x_train, _), (x_test, _) = mnist.load_data()
+
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+
+x_train = np.reshape(x_train, ((x_train.shape[0],) + shape_ord))
+x_test = np.reshape(x_test, ((x_test.shape[0],) + shape_ord))
+
+
+from keras.callbacks import TensorBoard
+
+
+batch_size=128
+steps_per_epoch = np.int(np.floor(x_train.shape[0] / batch_size))
+conv_autoencoder.fit(x_train, x_train, epochs=50, batch_size=128,
+                     shuffle=True, validation_data=(x_test, x_test),
+                     callbacks=[TensorBoard(log_dir='./tf_autoencoder_logs')])
+
+
+decoded_imgs = conv_autoencoder.predict(x_test)
+
+n = 10
+plt.figure(figsize=(20, 4))
+for i in range(n):
+    # display original
+    ax = plt.subplot(2, n, i+1)
+    plt.imshow(x_test[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # display reconstruction
+    ax = plt.subplot(2, n, i + n + 1)
+    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()
